8afcc9d6ba35a51c5c9b5c30d393b882ad04d294
tonystratum <str4tum@gmail.com>
2020-08-23 15:42:23 +0300

    First commit to conv_layer branch Contains my implementation of the convolutional layer forward propagation via the matmul approach. im2col and col2im courtesy of Stanford`s CS213N, and a kind stranger from StackOverflow.

---
+++ convolve_2d.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from im2col import im2col_sliding_strided
+
+
+def convolve_2d(input, W, stride=1):
+    output_shape = (input.shape[-1] - W[0].shape[0]) // stride + 1
+
+    output = []
+
+    for filtered in np.split(input, input.shape[0], axis=0):
+        img = filtered.reshape(
+            filtered.shape[-2],
+            filtered.shape[-1]
+        )
+
+        img_ = im2col_sliding_strided(img, W[0].shape, stride)  # is the one
+
+        W_flattened = np.concatenate([np.reshape(filter, (1, -1)) for filter in W], axis=0)
+
+        a_ = np.dot(W_flattened, img_)
+        a = a_.copy()
+        a.resize((len(W), output_shape, output_shape))
+        output.append(a)
+
+    return np.concatenate(output, axis=0)
---
+++ max_pool.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from im2col import im2col_sliding_strided
+
+
+def max_pool_greedy(input):
+    """
+    Returns a maximum value for each 2x2 FOV with a stride of 2.
+    (Looped implementation)
+    :param input: numpy.ndarray of shape (n, n)
+    :return: pooled numpy.ndarray of shape (n // 2, n // 2)
+    """
+    orig_shape = input.shape
+    pad_shape = [s + 1 for s in input.shape]
+    new_shape = [s // 2 for s in pad_shape]
+
+    input_padded = np.zeros(pad_shape)
+    input_padded[:input.shape[0], :input.shape[1]] = input
+    input_str = im2col_sliding_strided(input_padded, (2, 2), stepsize=1)
+
+    aaa = np.hsplit(input_str, orig_shape[0])
+    skipped = [array[:, ::2] for array in aaa[::2]]
+    proc_conc = np.concatenate(skipped, axis=1)
+    pooled = np.max(proc_conc, axis=0)
+    final = np.reshape(pooled, new_shape)
+    return final
+
+
+def max_pool_not_greedy(input):
+    """
+    Returns a maximum value for each 2x2 FOV with a stride of 2.
+    (Unlooped implementation)
+    :param input: numpy.ndarray of shape (n, n)
+    :return: pooled numpy.ndarray of shape (n // 2, n // 2)
+    """
+    orig_shape = input.shape
+    pad_shape = (input.shape[0] + 1, input.shape[1] + 1)
+    new_shape = (pad_shape[0] // 2, pad_shape[1] // 2)
+
+    input_padded = np.zeros(pad_shape)
+    input_padded[:input.shape[0], :input.shape[1]] = input
+    input_str = im2col_sliding_strided(input_padded, (2, 2), stepsize=1)
+
+    aaa = np.hsplit(input_str, orig_shape[0])
+    skipped = np.stack(aaa, axis=0)[::2, :, ::2]
+    proc_conc = np.concatenate(skipped[:], axis=1)
+    pooled = np.max(proc_conc, axis=0)
+    final = np.reshape(pooled, new_shape)
+    return final
